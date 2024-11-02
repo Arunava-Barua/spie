e8907ea636892f223e3fbae576c31da2509e8bee
Arunava-Barua <rahulbarua31@gmail.com>
2024-11-02 13:28:07 +0530

    Working code

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,27 +1,42 @@
 import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 import { mockTransactions } from "../../data/mockData";
+
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
-import EmailIcon from "@mui/icons-material/Email";
-import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
+import Security from "@mui/icons-material/Security";
+import AddModeratorIcon from "@mui/icons-material/AddModerator";
+import GppGoodIcon from "@mui/icons-material/GppGood";
+import SafetyCheckIcon from "@mui/icons-material/SafetyCheck";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import PolicyIcon from "@mui/icons-material/Policy";
+import VerifiedUserIcon from "@mui/icons-material/VerifiedUser";
+import LocalPoliceIcon from "@mui/icons-material/LocalPolice";
+import GppMaybeIcon from "@mui/icons-material/GppMaybe";
+import AssuredWorkloadIcon from "@mui/icons-material/AssuredWorkload";
+import PrivacyTipIcon from "@mui/icons-material/PrivacyTip";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import SendIcon from "@mui/icons-material/Send";
+import PersonIcon from "@mui/icons-material/Person";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+
 import Header from "../../components/Header";
-import LineChart from "../../components/LineChart";
-import GeographyChart from "../../components/GeographyChart";
-import BarChart from "../../components/BarChart";
 import StatBox from "../../components/StatBox";
-import ProgressCircle from "../../components/ProgressCircle";
+
+import { useContext, useEffect, useState } from "react";
+import SpieContext from "../../context/SpieContext";
 
 const Dashboard = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
+  const { details } = useContext(SpieContext);
+
   return (
-    <Box m='20px'>
+    <Box m="20px">
       {/* HEADER */}
-      <Box display='flex' justifyContent='space-between' alignItems='center'>
-        <Header title='DASHBOARD' subtitle='Welcome to your dashboard' />
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Analytics" />
 
         <Box>
           <Button
@@ -42,32 +57,31 @@ const Dashboard = () => {
       {/* GRID & CHARTS */}
 
       {/* 1 row */}
-
       <Box
-        display='flex'
+        display="flex"
         // flexDirection='row'
         // flexWrap='wrap'
-        gap='20px'
-        justifyContent='space-between'
+        gap="20px"
+        justifyContent="space-between"
       >
         {/* ROW 1 */}
         <Box
-          flex='1 1 calc(75% - 20px)' // 3-column span equivalent (taking 75% of the row width)
+          flex="1 1 calc(75% - 20px)" // 3-column span equivalent (taking 75% of the row width)
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          paddingTop='25px'
-          paddingBottom='25px'
-          minWidth='250px' // Optional: sets a minimum width to avoid shrinking too small on narrow screens
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          paddingTop="25px"
+          paddingBottom="25px"
+          minWidth="250px" // Optional: sets a minimum width to avoid shrinking too small on narrow screens
         >
           <StatBox
-            title='12,361'
-            subtitle='Emails Sent'
-            progress='0.79'
-            increase='+14%'
+            title={`${details.txScore} ${details.txSeverity}`}
+            subtitle="Transaction Severity Score"
+            progress={details.txScore/100}
+            // increase='+14%'
             icon={
-              <EmailIcon
+              <Security
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -75,20 +89,24 @@ const Dashboard = () => {
         </Box>
 
         <Box
-          flex='1 1 calc(75% - 20px)'
+          flex="1 1 calc(75% - 20px)"
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          minWidth='250px'
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
         >
           <StatBox
-            title='431,225'
-            subtitle='Sales Obtained'
-            progress='0.50'
-            increase='+21%'
+            title={
+              details.from.slice(0, 5) +
+              "..." +
+              details.from.slice(-3)
+            }
+            subtitle="From"
+            progress="0"
+            // increase='+21%'
             icon={
-              <PointOfSaleIcon
+              <SendIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -96,20 +114,20 @@ const Dashboard = () => {
         </Box>
 
         <Box
-          flex='1 1 calc(75% - 20px)'
+          flex="1 1 calc(75% - 20px)"
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          minWidth='250px'
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
         >
           <StatBox
-            title='32,441'
-            subtitle='New Clients'
-            progress='0.30'
-            increase='+5%'
+            title={details.amount}
+            subtitle="Amount"
+            progress="0"
+            // increase='+5%'
             icon={
-              <PersonAddIcon
+              <MonetizationOnIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -117,20 +135,24 @@ const Dashboard = () => {
         </Box>
 
         <Box
-          flex='1 1 calc(75% - 20px)'
+          flex="1 1 calc(75% - 20px)"
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          minWidth='250px'
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
         >
           <StatBox
-            title='1,325,134'
-            subtitle='Traffic Received'
-            progress='0.80'
-            increase='+43%'
+            title={
+              details.to.slice(0, 5) +
+              "..." +
+              details.to.slice(-3)
+            }
+            subtitle="To"
+            progress="0"
+            // increase='+43%'
             icon={
-              <TrafficIcon
+              <PersonIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -138,34 +160,33 @@ const Dashboard = () => {
         </Box>
       </Box>
 
-      <Header title='DASHBOARD' subtitle='Welcome to your dashboard' />
-
       {/* 2nd row */}
+      <Header title="Advanced Wallet Analytics" />
       <Box
-        display='flex'
+        display="flex"
         // flexDirection='row'
         // flexWrap='wrap'
-        gap='20px'
-        justifyContent='space-between'
+        gap="20px"
+        justifyContent="space-between"
       >
         {/* ROW 1 */}
         <Box
-          flex='1 1 calc(75% - 20px)' // 3-column span equivalent (taking 75% of the row width)
+          flex="1 1 calc(75% - 20px)" // 3-column span equivalent (taking 75% of the row width)
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          paddingTop='25px'
-          paddingBottom='25px'
-          minWidth='250px' // Optional: sets a minimum width to avoid shrinking too small on narrow screens
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          paddingTop="25px"
+          paddingBottom="25px"
+          minWidth="250px" // Optional: sets a minimum width to avoid shrinking too small on narrow screens
         >
           <StatBox
-            title='12,361'
-            subtitle='Emails Sent'
-            progress='0.79'
-            increase='+14%'
+            title={details.walletScore}
+            subtitle="Wallet Score"
+            progress={details.walletScore/100}
+            // increase='+14%'
             icon={
-              <EmailIcon
+              <AddModeratorIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -173,20 +194,20 @@ const Dashboard = () => {
         </Box>
 
         <Box
-          flex='1 1 calc(75% - 20px)'
+          flex="1 1 calc(75% - 20px)"
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          minWidth='250px'
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
         >
           <StatBox
-            title='431,225'
-            subtitle='Sales Obtained'
-            progress='0.50'
-            increase='+21%'
+            title={details.walletSeverity}
+            subtitle="Wallet Severity"
+            progress={details.walletScore/100}
+            // increase='+21%'
             icon={
-              <PointOfSaleIcon
+              <GppGoodIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -194,20 +215,20 @@ const Dashboard = () => {
         </Box>
 
         <Box
-          flex='1 1 calc(75% - 20px)'
+          flex="1 1 calc(75% - 20px)"
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          minWidth='250px'
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
         >
           <StatBox
-            title='32,441'
-            subtitle='New Clients'
-            progress='0.30'
-            increase='+5%'
+            title={details.walletEntityType ? details.walletEntityType : "NULL"}
+            subtitle="Entity Type"
+            progress="0"
+            // increase='+5%'
             icon={
-              <PersonAddIcon
+              <GppMaybeIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -215,20 +236,20 @@ const Dashboard = () => {
         </Box>
 
         <Box
-          flex='1 1 calc(75% - 20px)'
+          flex="1 1 calc(75% - 20px)"
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          minWidth='250px'
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
         >
           <StatBox
-            title='1,325,134'
-            subtitle='Traffic Received'
-            progress='0.80'
-            increase='+43%'
+            title={details.walletEntityName ? details.walletEntityName : "NULL"}
+            subtitle="Entity Name"
+            progress="0"
+            // increase='+43%'
             icon={
-              <TrafficIcon
+              <LocalPoliceIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -237,32 +258,32 @@ const Dashboard = () => {
       </Box>
 
       {/* 3rd row */}
-      <Header title='DASHBOARD' subtitle='Welcome to your dashboard' />
+      <Header title="Wallet Analysis (Transactions)" />
       <Box
-        display='flex'
+        display="flex"
         // flexDirection='row'
         // flexWrap='wrap'
-        gap='20px'
-        justifyContent='space-between'
+        gap="20px"
+        justifyContent="space-between"
       >
         {/* ROW 1 */}
         <Box
-          flex='1 1 calc(75% - 20px)' // 3-column span equivalent (taking 75% of the row width)
+          flex="1 1 calc(75% - 20px)" // 3-column span equivalent (taking 75% of the row width)
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          paddingTop='25px'
-          paddingBottom='25px'
-          minWidth='250px' // Optional: sets a minimum width to avoid shrinking too small on narrow screens
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          paddingTop="25px"
+          paddingBottom="25px"
+          minWidth="250px" // Optional: sets a minimum width to avoid shrinking too small on narrow screens
         >
           <StatBox
-            title='12,361'
-            subtitle='Emails Sent'
-            progress='0.79'
-            increase='+14%'
+            title={details.walletTxScore.toFixed(2)}
+            subtitle="Wallet Transaction Score"
+            progress={details.walletTxScore/100}
+            // increase='+14%'
             icon={
-              <EmailIcon
+              <VerifiedUserIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -270,20 +291,20 @@ const Dashboard = () => {
         </Box>
 
         <Box
-          flex='1 1 calc(75% - 20px)'
+          flex="1 1 calc(75% - 20px)"
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          minWidth='250px'
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
         >
           <StatBox
-            title='431,225'
-            subtitle='Sales Obtained'
-            progress='0.50'
-            increase='+21%'
+            title={details.walletTxAct}
+            subtitle="Transaction Activity Score"
+            progress={details.walletTxAct == "N/A" ? "0" : details.walletTxAct/5}
+            // increase='+21%'
             icon={
-              <PointOfSaleIcon
+              <PolicyIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -291,20 +312,20 @@ const Dashboard = () => {
         </Box>
 
         <Box
-          flex='1 1 calc(75% - 20px)'
+          flex="1 1 calc(75% - 20px)"
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          minWidth='250px'
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
         >
           <StatBox
-            title='32,441'
-            subtitle='New Clients'
-            progress='0.30'
-            increase='+5%'
+            title={details.walletMalInt}
+            subtitle="Malicious Interaction Score"
+            progress={details.walletMalInt == "N/A" ? "0" : details.walletMalInt/5}
+            // increase='+5%'
             icon={
-              <PersonAddIcon
+              <HealthAndSafetyIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -312,18 +333,115 @@ const Dashboard = () => {
         </Box>
 
         <Box
-          flex='1 1 calc(75% - 20px)'
+          flex="1 1 calc(75% - 20px)"
           backgroundColor={colors.primary[400]}
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          minWidth='250px'
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
         >
           <StatBox
-            title='1,325,134'
-            subtitle='Traffic Received'
-            progress='0.80'
-            increase='+43%'
+            title={details.walletCCScore}
+            subtitle="Cross-Chain Activity Score"
+            progress={details.walletCCScore == "N/A" ? "0" : details.walletCCScore/5}
+            // increase='+43%'
+            icon={
+              <SafetyCheckIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+      </Box>
+
+      {/* 4rd row */}
+      <Header title="Wallet Analysis (Balance)" />
+      <Box
+        display="flex"
+        // flexDirection='row'
+        // flexWrap='wrap'
+        gap="20px"
+        justifyContent="space-between"
+      >
+        {/* ROW 1 */}
+        <Box
+          flex="1 1 calc(75% - 20px)" // 3-column span equivalent (taking 75% of the row width)
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          paddingTop="25px"
+          paddingBottom="25px"
+          minWidth="250px" // Optional: sets a minimum width to avoid shrinking too small on narrow screens
+        >
+          <StatBox
+            title={details.muleSus}
+            subtitle="Mule Suspicion"
+            progress="0"
+            // increase='+14%'
+            icon={
+              <AssuredWorkloadIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        <Box
+          flex="1 1 calc(75% - 20px)"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
+        >
+          <StatBox
+            title={details.balVelocity}
+            subtitle="Balance Velocity"
+            progress={details.balVelocity == "N/A" ? "0" : details.balVelocity/5}
+            // increase='+21%'
+            icon={
+              <PrivacyTipIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        <Box
+          flex="1 1 calc(75% - 20px)"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
+        >
+          <StatBox
+            title={details.walletContractInt}
+            subtitle="Contract-Interaction score"
+            progress={details.walletContractInt == "N/A" ? "0" : details.walletContractInt/5}
+            // increase='+5%'
+            icon={
+              <AdminPanelSettingsIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        <Box
+          flex="1 1 calc(75% - 20px)"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minWidth="250px"
+        >
+          <StatBox
+            title={details.iqr}
+            subtitle="Interquartile Balance Count (IQR)"
+            progress={details.iqr == "N/A" ? "0" : details.iqr/5}
+            // increase='+43%'
             icon={
               <TrafficIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
